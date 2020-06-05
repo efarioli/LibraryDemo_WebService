9a4404978fc6c845be0c803351915002ccc7ef58
Ezequiel Farioli <efarioli@gmail.com>
2020-06-05 12:36:36 +0100

    create findMemberByUserId  Method in MemberGateway class

--- src/java/dbase/LibrarianGateway.java
+++ src/java/dbase/LibrarianGateway.java
@@ -1,6 +1,7 @@
 package dbase;
 
 import dto.LibrarianDTO;
+import dto.MemberDTO;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -10,39 +11,56 @@ import java.sql.SQLException;
  *
  * @author gdm1
  */
-public class LibrarianGateway
-{
+public class LibrarianGateway {
 
     private static final String LIBRARIAN_WITH_USERNAME = "SELECT * FROM Librarian WHERE username = ?";
+    private static final String MEMBER_WITH_ID = "SELECT * FROM Member WHERE id = ?";
 
-    public LibrarianDTO findLibrarianByUsername(String username)
-    {
+    public LibrarianDTO findLibrarianByUsername(String username) {
         LibrarianDTO librarian = null;
         Connection con = DB_Manager.getConnection();
-        try
-        {
+        try {
             PreparedStatement stmt = con.prepareStatement(LIBRARIAN_WITH_USERNAME);
             stmt.setString(1, username);
             ResultSet rs = stmt.executeQuery();
-            
-            if (rs.next())
-            {
+
+            if (rs.next()) {
                 librarian = new LibrarianDTO(rs.getInt("ID"),
-                                       rs.getString("LIBRARIANNAME"),
-                                       rs.getString("USERNAME"),
-                                       rs.getString("PWD"));
+                        rs.getString("LIBRARIANNAME"),
+                        rs.getString("USERNAME"),
+                        rs.getString("PWD"));
             }
-            
+
             con.close();
-        }
-        catch (NullPointerException npe)
-        {
+        } catch (NullPointerException npe) {
             System.err.println("No connection available");
-        }
-        catch (SQLException sqle)
-        {
+        } catch (SQLException sqle) {
             System.err.println(sqle.getMessage());
         }
         return librarian;
+    }
+
+    public MemberDTO findMemberByUserId(int userId) {
+        MemberDTO member = null;
+        Connection con = DB_Manager.getConnection();
+        try {
+            PreparedStatement stmt = con.prepareStatement(MEMBER_WITH_ID);
+            stmt.setInt(1, userId);
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                member = new MemberDTO(rs.getInt("ID"),
+                        rs.getString("MEMBERNAME"),
+                        rs.getString("USERNAME"),
+                        rs.getString("PWD"));
+            }
+
+            con.close();
+        } catch (NullPointerException npe) {
+            System.err.println("No connection available");
+        } catch (SQLException sqle) {
+            System.err.println(sqle.getMessage());
+        }
+        return member;
     }
 }
